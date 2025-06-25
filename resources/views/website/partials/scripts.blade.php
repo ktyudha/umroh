7e7e62e50d95b3db8df4438ac257eca00471b57d
ktyudha <ktyudha@gmail.com>
2025-06-25 11:35:36 +0700

    feat: update script

--- resources/views/website/partials/scripts.blade.php
+++ resources/views/website/partials/scripts.blade.php
@@ -1,3 +1,3 @@
-{{-- <script src="{{ asset('static/website/js/main.js') }}"></script> --}}
-{{-- <script src="{{ asset('static/website/js/home.js') }}"></script> --}}
+<script src="{{ asset('static/website/js/main.js') }}"></script>
+<script src="{{ asset('static/website/js/home.js') }}"></script>
 {{-- <script src="{{ asset('static/website/js/form-validation.js') }}"></script> --}}
