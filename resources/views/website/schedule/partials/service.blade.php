2aebc352c2fc128007af124220c8f0a7df8127bb
ktyudha <ktyudha@gmail.com>
2025-07-10 12:26:04 +0700

    feat: update float

--- resources/views/website/schedule/partials/service.blade.php
+++ resources/views/website/schedule/partials/service.blade.php
@@ -10,22 +10,5 @@
         Informasi Paket</button>
 </section>
 
-<div class="fixed bottom-4 left-4 right-4 flex flex-col gap-2 sm:hidden z-10">
-    <!-- Tombol WhatsApp -->
-    <a href="https://api.whatsapp.com/send?phone={{ @$setting->firstWhere('key', 'whatsapp')->value }}&text={{ route('schedule.show', $schedule->slug) }}"
-        target="_blank"
-        class="bg-[#25D366] flex items-center justify-center gap-2 w-full rounded-lg py-2 text-white font-medium">
-        <i class="fa-brands fa-whatsapp"></i>
-        <span>Tanya CS</span>
-    </a>
-
-    <!-- Tombol Informasi Paket -->
-    <button type="button"
-        class="w-full bg-[#17a2b8] py-2 rounded-lg text-white font-medium flex items-center justify-center gap-2"
-        data-modal-target="hotel-modal-detail" data-modal-toggle="hotel-modal-detail">
-        <i class="fa-regular fa-eye"></i>
-        <span>Informasi Paket</span>
-    </button>
-</div>
 
 @include('website.schedule.partials.modal-package')
